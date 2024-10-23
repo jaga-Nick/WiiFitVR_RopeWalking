f825541f1740148ae5aa2e6c42e5bba317484c38
jaga-Nick <shrimp1059yt@gmail.com>
2024-10-23 15:03:37 +0900

    2024/10/23矢野：追加

--- WiiFitVR/Assets/Main/Script/EmemyCon.cs
+++ WiiFitVR/Assets/Main/Script/EmemyCon.cs
@@ -15,4 +15,25 @@ public class EmemyCon : MonoBehaviour
     {
         transform.Rotate(Vector3.up * 1000 * Time.deltaTime);
     }
+
+    public float knockbackForce = 10f;  // 吹っ飛ばす力
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // プレイヤーにタグ "Player" がついている前提
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // プレイヤーの Rigidbody を取得
+            Rigidbody playerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+
+            if (playerRigidbody != null)
+            {
+                // 衝突の接触点の法線を取得（衝突方向）
+                Vector3 knockbackDirection = collision.contacts[0].normal;
+
+                // プレイヤーを反対方向に吹っ飛ばす
+                playerRigidbody.AddForce(-knockbackDirection * knockbackForce, ForceMode.Impulse);
+            }
+        }
+    }
 }
