209c1b138cc33aca6c7a6ae56bf61321046c39a0
kumabuchi-takahiro <kumaboo0701@gmail.com>
2024-11-01 16:55:23 +0900

    buchiフォルダにてVRデバック用のオブジェクト追加

---
+++ WiiFitVR/Assets/buchi/scripts/JumpSetteing.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class JumpSetteing : MonoBehaviour
+{
+    public GameObject centerEyeAnchor;
+    private float initialYPosition;
+    private bool isCrouched;
+    private bool isGrounded = true;  // 地面にいるかどうか
+    private bool isRope = true; //ロープにいるかどうか
+    private Rigidbody rb;
+    private bool hasInitialized = false;
+    private int framesToWait = 10; // 10フレーム待機
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        isCrouched = false;
+    }
+
+    void Update()
+    {
+        if (!hasInitialized)
+        {
+            if (framesToWait > 0)
+            {
+                framesToWait--;
+                return; // フレーム待機
+            }
+            initialYPosition = centerEyeAnchor.transform.localPosition.y;
+            hasInitialized = true;
+            Debug.Log("初期Y位置設定完了: " + initialYPosition);
+        }
+
+        float currentYPosition = centerEyeAnchor.transform.localPosition.y;
+
+        // しゃがみ検出（地面にいるときのみ）
+        if ((isRope || isGrounded) && initialYPosition > currentYPosition + 0.2f)
+        {
+            isCrouched = true;
+        }
+        else if ((isRope || isGrounded) && isCrouched && currentYPosition + 0.05f >= initialYPosition)
+        {
+            Jump();
+            isCrouched = false;
+        }
+    }
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * 80.0f, ForceMode.Impulse);
+        isGrounded = false;  // ジャンプしたら地面を離れる
+        isRope = false;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 地面に接触したらisGroundedをtrueにする
+        if (collision.gameObject.CompareTag("Ground"))  // 地面のタグが"Ground"であると仮定
+        {
+            isGrounded = true;
+        }
+        if (collision.gameObject.CompareTag("Rope"))  // 地面のタグが"Ground"であると仮定
+        {
+            isRope = true;
+        }
+    }
+}
